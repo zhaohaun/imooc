25279b8f3502b0aeb6fbf8fc7fa4d004a1a5fbde
zhaohuan <449162701@qq.com>
2018-03-18 12:20:15 +0800

    04
    Experiments

---
+++ dreamly/Experiments/Main3.java
@@ -0,0 +1,23 @@
+package dreamly.timeComplexicity.Experiments;
+
+public class Main3 {
+	
+	public static void main(String[] args){
+        
+		//数据规模倍乘测试binarySearch
+        //O（logn）
+        System.out.println("Test for findMax:");
+        for( int i = 10 ; i <= 28 ; i ++){
+            
+            int n = (int)Math.pow(2, i);
+            Integer[] arr = MyUtil.generateOrderedArray(n);
+            
+            long startTime = System.currentTimeMillis();
+            MyAlgorithmTester.binarySearch(arr, n, 0);
+            long endTime = System.currentTimeMillis();
+            
+            System.out.println("data size 2^" + i + " = " + n + "\t");
+            System.out.println("Time cost:" + (endTime - startTime) + " ms");
+      }
+	}
+}
---
+++ dreamly/Experiments/Main4.java
@@ -0,0 +1,22 @@
+package dreamly.timeComplexicity.Experiments;
+
+public class Main4 {
+	public static void main(String[] args) {
+
+		//数据规模倍乘测试mergeSort
+        //O（nlogn）
+		System.out.println("Test for findMax:");
+		for (int i = 10; i <= 26; i++) {
+
+			int n = (int) Math.pow(2, i);
+			Integer[] arr = MyUtil.generateRandomArray(n, 0, 1<<30);//1*2^30=1073741824
+
+			long startTime = System.currentTimeMillis();
+			MyAlgorithmTester.mergeSort(arr, n);
+			long endTime = System.currentTimeMillis();
+
+			System.out.println("data size 2^" + i + " = " + n + "\t");
+			System.out.println("Time cost:" + (endTime - startTime) + " ms");
+		}
+	}
+}
